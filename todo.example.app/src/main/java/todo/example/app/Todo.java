dd6ff8ae364fb2058626e1658358b3294bf5db10
serenajung <syjylp93@gmail.com>
2023-06-12 15:53:03 +0900

    commit final

---
+++ todo.example.app/src/main/java/todo/example/app/Todo.java
@@ -0,0 +1,12 @@
+package todo.example.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Todo {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application !";
+    }
+}
